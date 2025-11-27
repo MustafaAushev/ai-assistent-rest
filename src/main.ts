668376749713c94c66b6c0a358290db8d3747b89
m.aushev <maushev@hltc.ru>
2025-11-27 10:40:29 +0300

    docs

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { configDotenv } from 'dotenv';
+import { writeFileSync } from 'node:fs';
 
 configDotenv();
 
@@ -13,6 +14,9 @@ async function bootstrap() {
   const documentFactory = () => SwaggerModule.createDocument(app, config);
 
   SwaggerModule.setup('docs', app, documentFactory);
+
+  const document = SwaggerModule.createDocument(app, config);
+  writeFileSync('./swagger-spec.json', JSON.stringify(document));
 
   await app.listen(process.env.PORT ?? 3000, () => {
     console.log(`Listening on port ${process.env.PORT ?? 3000}`);
